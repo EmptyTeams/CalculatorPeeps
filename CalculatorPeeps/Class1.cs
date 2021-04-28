439f396c18727c9808b90691a76ea921320d0aad
aiden lundeen <aiden.lundeen@student.centralia.edu>
2021-04-28 08:52:12 -0700

    Add Multiply+Divide Methods

--- CalculatorPeeps/Class1.cs
+++ CalculatorPeeps/Class1.cs
@@ -2,5 +2,29 @@
 
 namespace CalculatorPeeps
 {
+    public static class Calculator
+    {
+        // Multiply-Divide
+        static int MaxValue = 2147483647;
+        static int Multiply(int one, int two)
+        {
+            if (!((Double)(one * two) == MaxValue
+                && 
+                !((Double)(one * two) == -MaxValue)))
+            { return one * two; }
 
+            return 0;
+        }
+
+        static int Divide(int Numerator, int Divisor)
+        {
+            if (Divisor == 0)
+            { throw new ArgumentOutOfRangeException("Divisor is 0"); }
+
+            return Numerator / Divisor;
+        }
+
+        //Add-Subtract
+
+    }
 }
